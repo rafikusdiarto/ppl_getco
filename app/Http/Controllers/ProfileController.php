a1a34073b718d3f6e500f68625b3b2f53d43bc32
Faisal Bahari <fbhr1112@gmail.com>
2023-05-28 15:15:53 +0700

    fix update profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,28 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\ClientData;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\ProfileUpdateRequest;
 
 class ProfileController extends Controller
 {
     public function show()
-    {
+    {   
         return view('auth.profile');
     }
 
     public function update(ProfileUpdateRequest $request)
     {
-        if ($request->password) {
-            auth()->user()->update(['password' => Hash::make($request->password)]);
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required:email', 
+            'nik' => 'required',
+            'phone' => 'required',
+            'address' => 'required',
+            'password' => 'required',
+        ]);
+        if(ClientData::where('user_id', auth()->user()->id)->exists()){
+            ClientData::where('user_id', auth()->user()->id)->update([
+                'nik' => $request->nik,
+                'phone' => $request->phone,
+                'address' => $request->address,
+            ]);
+        }else{
+            ClientData::create([
+                'user_id' => auth()->user()->id,
+                'nik' => $request->nik,
+                'phone' => $request->phone,
+                'address' => $request->address,
+            ]);
         }
-
-        auth()->user()->update([
+        User::where('id', auth()->user()->id)->update([
             'name' => $request->name,
             'email' => $request->email,
-            'nik' => $request->nik,
-            'phone' => $request->phone,
-            'address' => $request->address,
+            'password' => bcrypt($request->password),
+            
         ]);
 
         return redirect()->back()->with('success', 'Profile updated.');
